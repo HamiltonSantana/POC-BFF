5d12a60abb6d2e33658086196a790d8783ab08b6
Hamilton <hamiltonsds@examroom.ai>
2022-10-20 19:16:42 -0300

    add (bff-service) - starting bff service implementation.

---
+++ cli-bff/Service/Interfaces/IUserMService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IUserMService
+    {
+       Task GetUser(int id);
+    }
+}
---
+++ cli-bff/Service/UserMService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Service.Interfaces;
+
+namespace Service
+{
+    public class UserMService : IUserMService
+    {
+        public Task GetUser(int id)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
